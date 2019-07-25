bd587d2ed481e44e8a5fd853c8ecbb01e2d486ab
Ben Stanke <ben.stanke@me.com>
2019-07-24 22:41:18 -0700

    Made the GUI a bit more pretty. Added an Add page

---
+++ InventoryTracker/InventoryTracker/Properties/Edit_ItemWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InventoryTracker
+{
+    /// <summary>
+    /// Interaction logic for Edit_ItemWindow.xaml
+    /// </summary>
+    public partial class Edit_ItemWindow : Window
+    {
+        public Edit_ItemWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_button(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Submit_button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+
+}
